2fdb373eb90817578eada954fad56e1945989af7
g-jannat <gazi.jannatul.ferdous.t@gmail.com>
2026-09-10 16:53:16 +0600

    Fix base path for blank screen issue

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,7 +8,7 @@ import Home from "./pages/Home";
 
 function Router() {
   return (
-    <WouterRouter base="/portfolio">
+    <WouterRouter base="/">
       <Switch>
         <Route path={"/"} component={Home} />
         <Route path={"/404"} component={NotFound} />
